f2697fe470c01cd6519aca01e51d554757cc4299
Radik <Radik.app@yandex.ru>
2023-05-09 19:07:54 +0400

    4 лаба готова

--- four_lab/four_lab.py
+++ four_lab/four_lab.py
@@ -3,20 +3,6 @@ import numpy as np
 import math
 
 
-class Queue:
-    def __init__(self, id, type, length):
-        self.id = id
-        self.type = type
-        self.t_in = 0
-        self.start = 0
-        self.end = 0
-        self.t_out = 0  # время простоя
-        self.spent = math.ceil(length / 10)
-
-    def __repr__(self):
-        return f"{self.id:3} {self.t_in:5} {self.start:5} {self.end:5} {self.t_out:5} {self.spent:5}"
-
-
 class CountMath:
     def __init__(self, list_0, gl):
         self.req_list = gl
@@ -24,9 +10,6 @@ class CountMath:
         self.n_type = [0, 0, 0, 0]
 
     def count_average(self):
-        """
-        Подсчёт средней длины сообщений и вероятности поступления сообщения для потока заявок
-        """
         len_average = [0, 0, 0, 0]
         print("1 Item:")
         print("Средняя длина сообщений")
@@ -36,15 +19,12 @@ class CountMath:
                     self.n_type[i] += 1
                     len_average[i] += j.length
             len_average[i] /= self.n_type[i]
-            print(f"{i+1} {len_average[i]:.4}")
+            print(f"{i + 1} {len_average[i]:.4}")
         print("\nВероятность поступления сообщения")
-        [print(f"{i+1} {self.n_type[i] / 100:4}") for i in range(4)]
+        [print(f"{i + 1} {self.n_type[i] / 100:4}") for i in range(4)]
         print()
 
     def count_queue_stats(self):
-        """
-        Подсчёт характеристик очереди заявок
-        """
         lam = 0  # интенсивность поступления заявок
         wi = [0, 0, 0, 0]  # среднее время пребывания заявки i-го типа в очереди
         ti = [0, 0, 0, 0]  # средний промежуток времени между поступлением заявок i-го типа
@@ -76,7 +56,7 @@ class CountMath:
             ti[i] /= self.n_type[i]
             lambda_i[i] /= ti[i]
             li[i] = wi[i] * lambda_i[i]
-            print(f"{i+1} {li[i]:.04}")
+            print(f"{i + 1} {li[i]:.04}")
             print(f"w[i] {wi[i]} * lambda[i] {lambda_i[i]}")
             lam += self.n_type[i] * (i + 1) / 100
             mi[i] /= ui[i]
@@ -94,16 +74,31 @@ class CountMath:
         print()
         print("3 Item:")
         print("Среднее число заявок в системе")
-        [print(f"{j+1}. {self.n_type[j] * (j + 1) / 100}") for j in range(4)]
+        [print(f"{j + 1}. {self.n_type[j] * (j + 1) / 100}") for j in range(4)]
         print("Коэффициент загрузки оборудования заявками")
-        [print(f"{j+1}. {pi_i[j]:.5}") for j in range(4)]
+        [print(f"{j + 1}. {pi_i[j]:.5}") for j in range(4)]
         print("Коэффициент простоя")
-        [print(f"{j+1}. {ni[j]:.05}") for j in range(4)]
+        [print(f"{j + 1}. {ni[j]:.05}") for j in range(4)]
         print("Среднее время пребывания в системе")
-        [print(f"{j+1}. {ui[j]:.04}") for j in range(4)]
+        [print(f"{j + 1}. {ui[j]:.04}") for j in range(4)]
         print("Интенсивность обслуживания")
-        [print(f"{j+1}. {mi[j]:.04}") for j in range(4)]
+        [print(f"{j + 1}. {mi[j]:.04}") for j in range(4)]
         print(f"Коэффициент загрузки: \nR={r:.05}")
+
+
+class Queue:
+    def __init__(self, id, type, length):
+        self.id = id
+        self.type = type
+        self.t_in = 0
+        self.start = 0
+        self.end = 0
+        self.t_out = 0  # время простоя
+        self.spent = math.ceil(length / 10)
+
+    def __repr__(self):
+        return f"Тпи: {self.type} Момент появления в канале: {self.t_in:5}; Начало обслуживания: {self.start:5} " \
+               f"Конец обсулживания: {self.end:5} Время ожидания: {self.t_out:5} Время пребывания в канале {self.spent:5}"
 
 
 class InputQueue:
@@ -111,31 +106,16 @@ class InputQueue:
         self.arr = arr
 
     def process_req(self):
-        """
-        Обработка исходных заявок и регистрация их в очереди
-        :return: очередь заявок
-        """
         req_list = []
         t0 = math.ceil(self.arr[0].time * 60)
         for id, val in enumerate(self.arr):
             temp = Queue(id + 1, val.type, val.length)
-            if val.type == 1:
-                temp.t_in = t0
-                temp.start = t0 + 1
-                temp.end = temp.start + temp.spent
-                temp.t_out = temp.start - temp.t_in
-                t0 = temp.end + 1
-            else:
-                temp.t_in = t0
-                t0 += 1
+            temp.t_in = t0
+            temp.start = t0 + random.randint(0, 5)
+            temp.end = temp.start + temp.spent
+            temp.t_out = temp.start - temp.t_in
+            t0 = temp.end + 1
             req_list.append(temp)
-        for i in range(2, 5):
-            for j in req_list:
-                if j.type == i:
-                    j.start = t0
-                    j.end = j.start + j.spent
-                    j.t_out = j.start - j.t_in
-                    t0 = j.end + 1
 
         return req_list
 
@@ -185,8 +165,6 @@ def create_req():
 if __name__ == '__main__':
     arr = create_req()
     input_queue = InputQueue(arr)
-    for i in arr:
-        print(i)
     list_input = input_queue.process_req()
     [print(i) for i in list_input]
     cm = CountMath(arr, list_input)
